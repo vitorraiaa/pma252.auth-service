77919f781ad5a6ac12251cedc9c2b1a8aa782ea1
Humberto Sandmann <hsandmann@ieee.org>
2025-09-05 15:52:48 -0300

    openfeign

--- src/main/java/store/auth/AuthApplication.java
+++ src/main/java/store/auth/AuthApplication.java
@@ -2,7 +2,11 @@ package store.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = {
+	"store.account"
+})
 @SpringBootApplication
 public class AuthApplication {
 
--- src/main/java/store/auth/AuthService.java
+++ src/main/java/store/auth/AuthService.java
@@ -1,11 +1,34 @@
 package store.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import store.account.AccountController;
+import store.account.AccountIn;
 
 @Service
 public class AuthService {
 
+    @Autowired
+    private AccountController accountController;
+
     public String register(String name, String email, String password) {
+
+        // Salvar no servico de Account
+        accountController.create(AccountIn.builder()
+            .name(name)
+            .email(email)
+            .password(password)
+            .build()
+        );
+
+
+
+
+        // Gera um token
+
+        // Retorna o token
+
         return null;
     }
 
